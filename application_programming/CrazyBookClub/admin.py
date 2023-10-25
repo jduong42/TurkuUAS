00274abd6a015bb7108ba47d8dd5fe91b33394a6
Juha Duong <juha.duong@edu.turkuamk.fi>
2023-10-25 22:10:50 +0300

    Added review model and tested that it works in the admin site

--- application_programming/CrazyBookClub/admin.py
+++ application_programming/CrazyBookClub/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book
+from .models import Book, Review
 
 admin.site.register(Book)
+admin.site.register(Review)
--- application_programming/CrazyBookClub/models.py
+++ application_programming/CrazyBookClub/models.py
@@ -12,3 +12,16 @@ class Book(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Review(models.Model):
+    my_review = models.TextField(max_length=5000)
+    stars = models.IntegerField()
+    unfinished = models.BooleanField()
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.my_review[:50]}"
