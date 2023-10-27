8f3c80be70d0311eaca043e139446f3f0e89fba1
Juha Duong <juha.duong@edu.turkuamk.fi>
2023-10-27 12:15:31 +0300

    adding information to urls.py and added code to define new_book & new_reviews in the views.py file

--- application_programming/CrazyBookClub/urls.py
+++ application_programming/CrazyBookClub/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
     path('books/', views.books, name='books'),
 
     # Page that shows all the reviews.
-    path('reviews/', views.reviews, name='reviews')
+    path('reviews/', views.reviews, name='reviews'),
+
+    # Page for adding new book.
+    path('new_book/', views.new_book, name='new_book'),
+
+    # Page for adding new review for a book.
+    path('new_review/', views.new_review, name='new_review'),
 ]
--- application_programming/CrazyBookClub/views.py
+++ application_programming/CrazyBookClub/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Book, Review
+from .forms import BookForm, ReviewForm
 
 # Create your views here.
 
@@ -19,3 +20,34 @@ def reviews(request):
     reviews = Review.objects.order_by('date_added')
     context = {'reviews': reviews}
     return render(request, 'CrazyBookClub/reviews.html', context)
+
+def new_book(request):
+    # Add a new book.
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = BookForm()
+    else:
+        # POST data submitted; process data.
+        form = BookForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('CrazyBookClub:books')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'CrazyBookClub/new_book.html', context)
+
+def new_review(request):
+    # Add a new review.
+    if request.method != 'POST':
+        form = ReviewForm()
+    else:
+        # Post data submitted; process data.
+        form = ReviewForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('CrazyBookClub:books')
+        
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'CrazyBookClub/books.html')
