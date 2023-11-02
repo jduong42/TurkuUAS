84f21d964bd696a2ae34b01a13776c8e75b1ff89
DESKTOP-12326BJ\JuhaD <juhaduong@hotmail.com>
2023-11-02 19:45:36 +0200

    final commit, there are definitely some unsorted issues in edge cases, but its at least somewhat working!

--- Introduction_to_ICT_Projects/main.py
+++ Introduction_to_ICT_Projects/main.py
@@ -11,15 +11,14 @@
 # **************************************************************************** #
 
 from random_list import *
-#from search_element import *
-#from add_element import *
 
 # Made a randomized list with 30 elements as soon as program starts.
 
 random_list = randomize_list()
 
+# A Simple user interface for choosing an action.
+
 def user_interface():
-    # A Simple user interface for choosing an action.
 
     print("Please, select the following option:")
     print("Press 1 --> Quit program.")
@@ -31,42 +30,54 @@ def user_interface():
 
     user_input = int(input("Please choose your option: "))
 
+    # Checking that the input is in range and valid
+    
     if user_input not in range(1,7):
         print("Invalid input, please choose again.")
         user_interface()
     else:
         parsing_option(user_input)
 
+# Taking the return value of user_interface function and passing into parsing function
 def parsing_option(user_input):
-    # Taking the return value of user_interface function and passing into parsing function
     
       if user_input == 1:
         print('\n')
         exit()
       if user_input == 2:
-          # kommentti
+        # Function compares given value with the current list.
 
-          compare_elements(random_list)
-          user_interface()
+        compare_elements(random_list)
+        user_interface()
       if user_input == 3:
-          # New random_list will be returned, when element has been added to the existing list, thus creating a updated list.
+        # New random_list will be returned, when element has been added to the existing list, thus creating a updated list.
           
-          add_element(random_list)
-          print('\n')
-          user_interface()
+        add_element(random_list)
+        print('\n')
+        user_interface()
       if user_input == 4:
-          # kommentti
+        # Function removes an element based on index.
 
-          remove_element(random_list)
-          print('\n')
-          user_interface()
+        remove_element(random_list)
+        print('\n')
+        user_interface()
+      if user_input == 5:
+        # Function sorts the elements 1st strings, 2nd integers and 3rd floats.
+
+        custom_sort(random_list)
+        print('\n')
+        user_interface()
       if user_input == 6:
+        # Function prints the current list, but for some reason it does not work after sorting the list
+         
         print(random_list)
         print('\n')
         user_interface()
 
+# Asking the user, what element to delete based on the index
+
 def remove_element(list):
-   # Asking the user, what element to delete based on the index
+
 
     print("Here is the existing list in from of: index - value")
     for (i, item) in enumerate(list):
@@ -77,13 +88,13 @@ def remove_element(list):
 
     return(list)
 
+# Asking the user, what datatype of the element will be and then ask what value the user wants to give it
 
 def add_element(new_list):
-    # Asking the user, what datatype of the element will be
 
     data_type = input("What datatype you want the new element to be? Choose from: integer, float or string\n")
     
-    #Checking if the input requirements are met for new element:
+    # Checking if the input requirements are met for new element:
 
     if data_type not in ["Integer", "integer", "int", "Float", "float", "String", "string", "str"]:
         print("Invalid input, choose again.")
@@ -99,8 +110,9 @@ def add_element(new_list):
 
     return(new_list)
 
+# Function asks first what datatype user wants to search and after that ask what value in the specific datatype. Also, checks if datatype is correct or not.
+
 def compare_elements(compare_list):
-    # Function asks first what datatype user wants to search and after that ask what value in the specific datatype. Also, checks if datatype is correct or not.
   
     data_type = input("Type what datatype you would like to find from the list? Choose from: integer, float or string:\n")
 
@@ -122,10 +134,25 @@ def compare_elements(compare_list):
       for x in compare_list:
         if x == user_input:
             print("The value was found in the list\n")
-    else:
-       print("The value was not found in the list\n")
+
+# A sorting function that extract a value from each element in the list, which is used for comparison during the sorting process. Strings are given the highest priority, followed by integers and then floats, with other data types considered last. 
+
+def custom_sort(random_list):
+  def custom_key(value):
+      if isinstance(value, str):
+         return(0, value)
+      elif isinstance(value, int):
+         return(1, value)
+      elif isinstance(value, float):
+         return(2, value)
+      else:
+         return(3, value)
+
+  new_list = sorted(random_list, key=custom_key)
+
+  return (new_list)
+         
     
-      
 def main():
   user_interface()
 
