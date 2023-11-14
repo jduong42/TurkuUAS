ba53b235455640e11ef80c9d82be2d58e2e71662
Juha Duong <juha.duong@edu.turkuamk.fi>
2023-11-14 13:22:26 +0200

    added new links for topic page and also checking if the reviews are showing in the specific book

--- application_programming/new_try/crazy_book_club/CrazyBookClub/urls.py
+++ application_programming/new_try/crazy_book_club/CrazyBookClub/urls.py
@@ -7,5 +7,9 @@ from . import views
 app_name = 'CrazyBookClub'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index'),    
+    path('', views.index, name='index'),
+    # Page that shows all books.
+    path('books/', views.books, name='books'),
+    # Detail page for a single book entry.
+    path('books/<int:book_id>/', views.book, name='book'),
 ]
--- application_programming/new_try/crazy_book_club/CrazyBookClub/views.py
+++ application_programming/new_try/crazy_book_club/CrazyBookClub/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
 
 def index(request):
     # The home page for CrazyBookClub.
     return render(request, 'CrazyBookClub/index.html')
+
+def books(request):
+    # Show all books.
+    books = Book.objects.order_by('date_added')
+    context = {'books': books}
+    return render(request, 'CrazyBookClub/books.html', context)
+
+def book(request, book_id):
+    # Show a single book and all its entries.
+    book = Book.objects.get(id=book_id)
+    entries = book.review_set.order_by('-date_added')
+    context = {'book': book, 'entries': entries}
+    return render(request, 'CrazyBookClub/book.html', context)
