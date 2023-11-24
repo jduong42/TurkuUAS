40d2a9599d79b66e201e221d7bfb10533d924cbe
Juha Duong <juha.duong@edu.turkuamk.fi>
2023-11-24 14:28:39 +0200

    first commit to the boardgame project. Made the environment + started app in django + added basic models

---
+++ application_programming/boardgame/boardgames/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+# A game class
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        # Returns a string presentation of the model.
+
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        # Returns a string presentation of the model.
+
+        return self.name
+
+class Loan(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrowed_date = models.DateField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, black=True)
