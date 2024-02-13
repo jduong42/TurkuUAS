da28306fda9a7768b1b076268d54503cef6e033c
Juha Duong <juha.duong@edu.turkuamk.fi>
2024-02-13 11:08:15 +0200

    Did exercise 2 task 2, now focusing on task 3

--- object_oriented_programming/Assignment_3/exercise1_2.py
+++ object_oriented_programming/Assignment_3/exercise1_2.py
@@ -1,29 +1,41 @@
 class  NumberStats:
     def __init__(self):
-        #no need to add any new varibales here, just change the
-        #initialization of the self.numbers variable.
-        self.numbers = 0
+        self.numbers = []
 
-    def add_number(self, number:int):
-        pass
-
+    def add_number(self, number: int):
+        self.numbers.append(number)
+        
     def count_numbers(self):
-        pass
+        return len(self.numbers)
+    
+    def get_sum(self):
+        if self.numbers == 0:
+            return 0
+        else:
+            return sum(self.numbers)
+        
+    def average(self):
+        if self.numbers == 0:
+            return 0
+        else:
+            return sum(self.numbers) / len(self.numbers)
 
-    if __name__ == "__main__":
-        #Part 1 test prints:
-        """ stats = NumberStats()
-        stats.add_number(3)
-        stats.add_number(5)
-        stats.add_number(1)
-        stats.add_number(2)
-        print("Numbers added:", stats.count_numbers()) """
-        #Part 2 test prints:
-        """ stats = NumberStats()
-        stats.add_number(3)
-        stats.add_number(5)
-        stats.add_number(1)
-        stats.add_number(2)
-        print("Numbers added:", stats.count_numbers())
-        print("Sum of numbers:", stats.get_sum())
-        print("Mean of numbers:", stats.average()) """
+
+if __name__ == "__main__":
+#Part 1 test prints:
+    stats = NumberStats()
+    stats.add_number(3)
+    stats.add_number(5)
+    stats.add_number(1)
+    stats.add_number(2)
+    print("Numbers added:", stats.count_numbers())
+
+    #Part 2 test prints:
+    stats = NumberStats()
+    stats.add_number(3)
+    stats.add_number(5)
+    stats.add_number(1)
+    stats.add_number(2)
+    print("Numbers added:", stats.count_numbers())
+    print("Sum of numbers:", stats.get_sum())
+    print("Mean of numbers:", stats.average())
