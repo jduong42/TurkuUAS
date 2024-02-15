f7e9c7959c9f6c65eada45b1331848a995a52d58
Juha Duong <juha.duong@edu.turkuamk.fi>
2024-02-15 14:56:38 +0200

    Added second part of ex02 task 5

--- front_end_development/ex02/exercise_05.js
+++ front_end_development/ex02/exercise_05.js
@@ -7,3 +7,20 @@ function greet(name = 'Guest') {
 // Calling the function with and without a parameter
 greet('John');
 greet();
+
+// Function using rest parameters
+
+function sum(...numbers) {
+    let result = 0;
+    for (let number of numbers) {
+        result = result + number;
+    }
+    return result;
+}
+
+// Calling the function with different numbers of arguments
+
+console.log(sum(1, 2, 3)); // Output: 6
+console.log(sum(1, 2, 3, 4, 5)); // Output: 15
+console.log(sum(10)); // Output: 10
+console.log(sum()); // Output: 0 (no arguments)
